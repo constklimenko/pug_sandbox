cc4274087426686a01c8437763da41f629c0f290
narcom <narcomacer@gmail.com>
2020-07-20 16:05:26 +0500

    шаблон структуры папок

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const gulp = require('gulp');
+
+// gulp.task('hello', function(w) {
+//     console.log('hello world');
+//     w()
+// })
+
+const less = require('gulp-less');
+const authoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCss = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const ftp = require('vinyl-ftp');
+
+
+const pathName = 'helloworld';
+
+
+
+const config = {
+    path: {
+        less: `${pathName}/src/*.less`,
+        less2: `${pathName}/src/parts/*.less`,
+        html: `${pathName}/index.html`,
+
+    },
+    output: {
+        cssName: `bundle${pathName}min.css`,
+        path: `${pathName}`,
+        path_file: `${pathName}/index.html`,
+        path_file_css: `${pathName}/${pathName}.css`,
+        newHtml: `/tmp/fz3temp-2`
+    }
+};
+
+
+
+
+gulp.task('less', function() {
+    return gulp.src(config.path.less)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat(config.output.cssName))
+        .pipe(authoprefixer())
+        .pipe(cleanCss())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.output.path))
+        .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('push', function() {
+    return gulp.src(config.output.path_file).pipe(gulp.dest(config.output.newHtml));
+});
+
+gulp.task('pushCss', function() {
+    return gulp.src(config.output.path_file_css).pipe(gulp.dest(config.output.newHtml));
+});
+
+gulp.task('serve', (done) => {
+    browserSync.init({
+        server: {
+            baseDir: config.output.path
+        }
+    });
+    gulp.watch(config.path.less, gulp.series('less'));
+    gulp.watch(config.path.less2, gulp.series('less'));
+    //, 'push', 'pushCss'));
+    gulp.watch(config.path.html).on('change', () => {
+        browserSync.reload();
+        done();
+    });
+
+});
+
+
+const globs = [
+    './app/index.html',
+
+];
+
+
+
+gulp.task('default', gulp.series('less', 'serve'));
